e39879a205de92d956404b28af2f8f630cc2b33d
axiaoxin <254606826@qq.com>
2020-03-26 00:00:49 +0800

    支持验证码识别 closes #3
    默认触发验证码时，将验证码保存下来，提示用户需要人工处理，手动输入验证码后继续后续逻辑
    支持注册破解验证码的函数，注册后触发验证码时，优先使用注册的函数识别验证码，如果识别失败则仍然采用提示用户手动输入

--- login.go
+++ login.go
@@ -9,10 +9,14 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"math/big"
 	"net/http"
 	"net/url"
+	"os"
+	"path"
 	"regexp"
 	"strconv"
 	"strings"
@@ -73,20 +77,21 @@ func (w *Weibo) MobileLogin() error {
 	return nil
 }
 
-// PCLogin 电脑web登录
-// 登录后才能成功获取开放平台授权码和token
-func (w *Weibo) PCLogin() error {
-	preloginURL := "https://login.sina.com.cn/sso/prelogin.php?"
-	ssologinURL := "https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)"
-	// pinURL := "https://login.sina.com.cn/cgi/pin.php" // 登录验证码相关url
+// RegisterCrackPinFunc 注册验证码破解方法
+func (w *Weibo) RegisterCrackPinFunc(f ...CrackPinFunc) {
+	w.crackPinFuncs = append(w.crackPinFuncs, f...)
+}
 
+func (w *Weibo) preLogin() (*PreLoginResp, error) {
 	// 请求prelogin 获得 servertime, nonce, pubkey, rsakv
 	// 对账号进行base64编码 对应javascript中encodeURIComponent然后base64编码
-	su := base64.StdEncoding.EncodeToString([]byte(w.username))
+	preloginURL := "https://login.sina.com.cn/sso/prelogin.php?"
 	req, err := http.NewRequest("GET", preloginURL, nil)
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin NewRequest prelogin error")
+		return nil, errors.Wrap(err, "weibo PCLogin NewRequest prelogin error")
 	}
+
+	su := base64.StdEncoding.EncodeToString([]byte(w.username))
 	params := url.Values{
 		"entry":    {"weibo"},
 		"su":       {su},
@@ -100,23 +105,30 @@ func (w *Weibo) PCLogin() error {
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	resp, err := w.client.Do(req)
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin Do prelogin error")
+		return nil, errors.Wrap(err, "weibo preLogin Do prelogin error")
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin ReadAll prelogin error")
-	}
-	preLoginResp := &PreLoginResp{}
-	if err := json.Unmarshal(body, preLoginResp); err != nil {
-		return errors.Wrap(err, "weibo PCLogin Unmarshal preLoginResp error")
-	}
-	if preLoginResp.Retcode != 0 {
-		return errors.New("weibo PCLogin preLoginResp Retcode error:" + string(body))
+		return nil, errors.Wrap(err, "weibo preLogin ReadAll prelogin error")
 	}
 
-	// 请求ssologin
+	preLoginResp := &PreLoginResp{}
+	if err := json.Unmarshal(body, preLoginResp); err != nil {
+		return nil, errors.Wrap(err, "weibo preLogin Unmarshal preLoginResp error")
+	}
+
+	if preLoginResp.Retcode != 0 {
+		return nil, errors.New("weibo preLogin preLoginResp Retcode error:" + string(body))
+	}
+
+	return preLoginResp, nil
+}
+
+func (w *Weibo) ssoLogin(preLoginResp *PreLoginResp, pinCode string) (*SsoLoginResp, error) {
+	ssologinURL := "https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)"
+
 	// 拼接明文js加密文件中得到
 	encMsg := []byte(fmt.Sprint(preLoginResp.Servertime, "\t", preLoginResp.Nonce, "\n", w.passwd))
 	// 创建公钥
@@ -126,10 +138,11 @@ func (w *Weibo) PCLogin() error {
 	// 加密公钥
 	sp, err := rsa.EncryptPKCS1v15(rand.Reader, pubkey, encMsg)
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin EncryptPKCS1v15 error")
+		return nil, errors.Wrap(err, "weibo ssoLogin EncryptPKCS1v15 error")
 	}
 	// 将加密信息转换为16进制
 	hexsp := hex.EncodeToString([]byte(sp))
+	su := base64.StdEncoding.EncodeToString([]byte(w.username))
 	data := url.Values{
 		"entry":      {"account"},
 		"gateway":    {"1"},
@@ -152,28 +165,128 @@ func (w *Weibo) PCLogin() error {
 		"prelt":      {"95"},
 		"returntype": {"TEXT"},
 	}
-	req, err = http.NewRequest("POST", ssologinURL, strings.NewReader(data.Encode()))
+	// 添加验证码参数
+	if pinCode != "" {
+		data.Add("door", pinCode)
+	}
+	req, err := http.NewRequest("POST", ssologinURL, strings.NewReader(data.Encode()))
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin NewRequest ssologin error")
+		return nil, errors.Wrap(err, "weibo PCLogin NewRequest ssologin error")
 	}
 	req.Header.Set("User-Agent", w.userAgent)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err = w.client.Do(req)
+	resp, err := w.client.Do(req)
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin Do ssologin error")
+		return nil, errors.Wrap(err, "weibo PCLogin Do ssologin error")
 	}
 	defer resp.Body.Close()
 
-	body, err = ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return errors.Wrap(err, "weibo PCLogin ReadAll ssologin error")
+		return nil, errors.Wrap(err, "weibo PCLogin ReadAll ssologin error")
 	}
+
+	// 登录结果返回结构体
 	ssoLoginResp := &SsoLoginResp{}
 	if err := json.Unmarshal(body, ssoLoginResp); err != nil {
-		return errors.Wrap(err, "weibo PCLogin Unmarshal ssoLoginResp error")
+		return nil, errors.Wrap(err, "weibo PCLogin Unmarshal ssoLoginResp error")
 	}
-	if ssoLoginResp.Retcode != "0" {
-		return errors.New("weibo PCLogin ssoLoginResp Retcode error:" + string(body))
+
+	return ssoLoginResp, nil
+}
+
+// PCLogin 电脑web登录
+// 登录后才能成功获取开放平台授权码和token
+func (w *Weibo) PCLogin() error {
+
+	// 是否触发验证码
+	hasPinCode := false
+	// 验证码字符串
+	pinCode := ""
+
+	// 登录返回结果
+	var ssoLoginResp *SsoLoginResp
+
+LOGIN: // 登录label，正常登录时跳出破解验证码的循环
+	for {
+		// 触发验证码时进行破解，最终得到字符串验证码
+		if hasPinCode {
+			// 获取验证码图片
+			randNum := randInt(10000000, 100000000) // 8位的随机数
+			pinURL := fmt.Sprintf("https://login.sina.com.cn/cgi/pin.php?r=%d&s=0", randNum)
+			resp, err := w.client.Get(pinURL)
+			if err != nil {
+				// 获取验证码图片失败则登录失败
+				return errors.Wrap(err, "weibo PCLogin Get pinURL error")
+			}
+			defer resp.Body.Close()
+			pinPic := resp.Body
+
+			if len(w.crackPinFuncs) > 0 {
+				// 有破解方法则尝试破解
+				for _, crack := range w.crackPinFuncs {
+					pinCode, err = crack(pinPic)
+					// 破解失败尝试使用下一个crack
+					if err != nil {
+						log.Println("[ERROR] weibo PCLogin crack pin error:" + err.Error())
+						continue
+					}
+					// 破解成功推出破解循环
+					if pinCode != "" {
+						break
+					}
+				}
+			}
+			// 破解失败，人工处理
+			if err != nil || pinCode == "" {
+				log.Println("无法自动处理登录验证码，需人工处理. ")
+				// 没有注册破解方法时，保存图片,用户人工识别
+				pinFilename := path.Join(os.TempDir(), "weibo_pin.png")
+				pinFile, err := os.Create(pinFilename)
+				if err != nil {
+					return errors.Wrap(err, "weibo PCLogin Create png file error")
+				}
+				defer pinFile.Close()
+				if _, err := io.Copy(pinFile, pinPic); err != nil {
+					return errors.Wrap(err, "weibo PCLogin Copy pinPic to pinFile error")
+				}
+
+				// 等待用户输入验证码
+				log.Printf("请输入 %s 中的验证码:", pinFilename)
+				fmt.Scanln(&pinCode)
+				log.Println("正在登录...")
+			}
+		}
+
+		// 请求prelogin
+		preLoginResp, err := w.preLogin()
+		if err != nil {
+			return err
+		}
+
+		// 请求ssologin
+		ssoLoginResp, err = w.ssoLogin(preLoginResp, pinCode)
+		if err != nil {
+			return err
+		}
+
+		switch ssoLoginResp.Retcode {
+		case "0":
+			// 成功登录跳出循环
+			break LOGIN
+		case "101":
+			// 账号密码错误
+			return errors.New("weibo PCLogin ssoLoginResp Retcode 101, username or password error")
+		case "2070":
+			// 验证码错误
+			return errors.New("weibo PCLogin ssoLoginResp Retcode 2070, pin code error")
+		case "4049":
+			// 触发验证码登录
+			hasPinCode = true
+		default:
+			// 其他错误
+			return fmt.Errorf("weibo PCLogin ssoLoginResp Retcode error. %+v", ssoLoginResp)
+		}
 	}
 	return w.loginSucceed(ssoLoginResp)
 }
