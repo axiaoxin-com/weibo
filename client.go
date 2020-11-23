bcf4f185ff5c2eff1507c4d678ca3000dc257b35
axiaoxin <254606826@qq.com>
2020-11-23 10:18:52 +0800

    HTTPTimeout

--- client.go
+++ client.go
@@ -14,6 +14,9 @@ type RespError struct {
 	Request   string `json:"request"`
 }
 
+// HTTPTimeout 请求超时时间 默认 10 秒
+var HTTPTimeout time.Duration = time.Second * 10
+
 // Weibo 实例，在其上实现各类接口
 type Weibo struct {
 	client        *http.Client
@@ -38,7 +41,7 @@ func New(appkey, appsecret, username, passwd, redirecturi string) *Weibo {
 	jar, _ := cookiejar.New(nil)
 	client := &http.Client{
 		Jar:     jar,
-		Timeout: 30 * time.Second,
+		Timeout: HTTPTimeout,
 	}
 	return &Weibo{
 		client:      client,
